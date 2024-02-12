424504f5b7eeb63f899f3d2035e9476d07605c46
Dhruvil patel Ordex <144197809+Dhruvil-patel09@users.noreply.github.com>
2024-02-12 14:51:08 +0530

    create Register and Redirect Login Page using backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,11 +5,12 @@ import { Link, useNavigate } from "react-router-dom";
 import UploadImageToCloudinary from "../utils/uploadCloudinary";
 import { BASE_URL } from "../config";
 import { toast } from "react-toastify";
+import HashLoader from "react-spinners/HashLoader";
 
 const Signup = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   const [previewURL, setPreviewURL] = useState("");
-  const [loading, setloading] = usestate(false);
+  const [loading, setloading] = useState(false);
 
   const [formData, setFormData] = useState({
     name: "",
@@ -51,14 +52,15 @@ const Signup = () => {
         body: JSON.stringify(formData),
       });
 
-      const { message } = await res.join();
+      const { message } = await res.json();
       if (!res.ok) {
         throw new Error(message);
       }
 
       setloading(false);
       toast.success(message);
-      navigate("/login");
+      navigate("/Login");
+      window.scrollTo(0, 0);
     } catch (err) {
       toast.error(err.message);
       setloading(false);
@@ -157,9 +159,15 @@ const Signup = () => {
               </div>
 
               <div className="mb-5 flex items-center gap-3">
-                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-                  <img src={avatar} alt="" className="w-full rounded-full" />
-                </figure>
+                {selectedFile && (
+                  <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                    <img
+                      src={previewURL}
+                      alt=""
+                      className="w-full rounded-full"
+                    />
+                  </figure>
+                )}
 
                 <div className="relative w-[130px] h-[50px]">
                   <input
@@ -181,10 +189,15 @@ const Signup = () => {
 
               <div className="mt-7">
                 <button
+                  disabled={loading && true}
                   type="submit"
                   className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
                 >
-                  Sign Up
+                  {loading ? (
+                    <HashLoader size={35} color="ffffff" />
+                  ) : (
+                    "Sign Up"
+                  )}
                 </button>
               </div>
 
@@ -193,6 +206,7 @@ const Signup = () => {
                 <Link
                   to="/Login"
                   className="text-primaryColor font-medium ml-1"
+                  onClick={() => window.scrollTo(0, 0)}
                 >
                   Login
                 </Link>
--- frontend/src/utils/uploadCloudinary.js
+++ frontend/src/utils/uploadCloudinary.js
@@ -1,5 +1,5 @@
-const cloud_name = import.meta.env.VITE_CLOUD_NAME;
 const upload_preset = import.meta.env.VITE_UPLOAD_PRESET;
+const cloud_name = import.meta.env.VITE_CLOUD_NAME;
 
 const UploadImageToCloudinary = async (file) => {
   const uploadData = new FormData();
@@ -9,9 +9,9 @@ const UploadImageToCloudinary = async (file) => {
   uploadData.append("cloud_name", cloud_name);
 
   const res = await fetch(
-    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload`,
+    `https://api.cloudinary.com/v1_1/${cloud_name}/image/upload/`,
     {
-      method: "post",
+      method: "POST",
       body: uploadData,
     }
   );
