8cf0650fa09bfcd44e5e81c2f1975458a24c5341
Dhruvil patel Ordex <144197809+Dhruvil-patel09@users.noreply.github.com>
2024-02-15 18:42:41 +0530

    login functionality and home screen login user name show

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useContext } from "react";
 import logo from "../../assets/images/SmartHealthLogo.png";
-import userImg from "../../assets/images/avatar-icon.png";
 import { NavLink, Link } from "react-router-dom";
 import { BiMenu } from "react-icons/bi";
+import { authContext } from "../../context/AuthContext";
+import HashLoader from "react-spinners/HashLoader";
 
 const navLinks = [
   {
@@ -25,6 +26,7 @@ const navLinks = [
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
+  const { user, role, token } = useContext(authContext);
 
   const handleStickyHeader = () => {
     window.addEventListener("scroll", () => {
@@ -80,23 +82,32 @@ const Header = () => {
 
           {/*  ===================== Nav right ===========================*/}
           <div className="flex items-center gap-4">
-            <div className="hidden">
-              <Link to="/">
-                <figure className="w-[35px] h-[35px] rounded-full">
-                  <img
-                    src={userImg}
-                    className="w-full rounded-full"
-                    alt="userImg"
-                  />
-                </figure>
+            {token && user ? (
+              <div>
+                <Link
+                  to={`${
+                    role === "doctor"
+                      ? "/doctors/profile/me"
+                      : "/users/profile/me"
+                  }`}
+                >
+                  <figure className="w-[35px] h-[35px] rounded-full">
+                    <img
+                      src={user?.photo}
+                      className="w-full rounded-full"
+                      alt="userImg"
+                    />
+                  </figure>
+                  <h2>{user?.name}</h2>
+                </Link>
+              </div>
+            ) : (
+              <Link to="/login" onClick={() => window.scrollTo(0, 0)}>
+                <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
+                  Login
+                </button>
               </Link>
-            </div>
-
-            <Link to="/login" onClick={() => window.scrollTo(0, 0)}>
-              <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]">
-                Login
-              </button>
-            </Link>
+            )}
 
             <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className="w-6 h-6 cursor-pointer " />
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../config";
+import { toast } from "react-toastify";
+import { authContext } from "../context/AuthContext.jsx";
+import HashLoader from "react-spinners/HashLoader.js";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -7,10 +11,52 @@ const Login = () => {
     password: "",
   });
 
+  const [loading, setloading] = useState(false);
+  const navigate = useNavigate();
+  const { dispatch } = useContext(authContext);
+
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+  const submitHandler = async (event) => {
+    // console.log(formData);
+    event.preventDefault();
+    setloading(true);
 
+    try {
+      const res = await fetch(`${BASE_URL}/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await res.json();
+      if (!res.ok) {
+        throw new Error(result.message);
+      }
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, "login data");
+
+      setloading(false);
+      toast.success(result.message);
+      navigate("/home");
+      window.scrollTo(0, 0);
+    } catch (err) {
+      toast.error(err.message);
+      setloading(false);
+    }
+  };
   return (
     <section className="px-5 lg:px-0">
       <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
@@ -18,7 +64,7 @@ const Login = () => {
           Hello! <span className="text-primaryColor">Welcome</span> Back🎉
         </h3>
 
-        <form className="py-4 md:py-0" action="">
+        <form className="py-4 md:py-0" action="" onSubmit={submitHandler}>
           <div className="mb-5">
             <input
               type="email"
@@ -49,7 +95,7 @@ const Login = () => {
               type="submit"
               className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
             >
-              Login
+              {loading ? <HashLoader size={25} color="#fff" /> : "Login"}
             </button>
           </div>
 
