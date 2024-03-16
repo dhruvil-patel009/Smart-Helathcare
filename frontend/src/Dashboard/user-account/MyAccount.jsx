2e5b03eb5d3e887b2194360132889df7e38c913e
Dhruvil Patel <85049831+dhruvil-patel009@users.noreply.github.com>
2024-03-17 01:20:59 +0530

    My Account Page Working
    Add Loader, ErrorMessage and my Booking

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -6,6 +6,7 @@ import MyBookings from "./MyBookings";
 import Profile from "./profile";
 import useGetProfile from "../../hooks/userFetchData";
 import { BASE_URL } from "../../config";
+import Loading from "../../components/Loader/Loading";
 
 const MyAccount = () => {
   const { dispatch } = useContext(authContext);
@@ -27,71 +28,79 @@ const MyAccount = () => {
   return (
     <section>
       <div className="max-w-[1170px] px-5 mx-auto">
-        <div className="grid md:grid-cols-3 gap-10">
-          <div className="pb-[50px] px-[30px] rounded-md">
-            <div className="flex items-center justify-center">
-              <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
-                <img
-                  src={userImg}
-                  alt="userimg"
-                  className="w-full h-full rounded-full"
-                />
-              </figure>
+        {loading && !error && <Loading />}
+
+        {error && !loading && <Error errMessage={error} />}
+
+        {!loading && !error && (
+          <div className="grid md:grid-cols-3 gap-10">
+            <div className="pb-[50px] px-[30px] rounded-md">
+              <div className="flex items-center justify-center">
+                <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
+                  <img
+                    src={userImg}
+                    alt="userimg"
+                    className="w-full h-full rounded-full"
+                  />
+                </figure>
+              </div>
+
+              <div className="text-center mt-4">
+                <h3 className="text-[18px] leading-[30px] text-headingColor font-bold ">
+                  Dhruvil Patel
+                </h3>
+                <p className="text-textColor text-[15px] leading-6 font-medium">
+                  Example@gmail.com
+                </p>
+
+                <p className="text-textColor text-[15px] leading-6 font-medium">
+                  Blood Type:
+                  <span className="ml-2 text-headingColor text-[22px] leading-8">
+                    O+
+                  </span>
+                </p>
+              </div>
+
+              <div className="mt-[50px] md:mt-[100px]">
+                <button
+                  onClick={handlelogout}
+                  className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
+                >
+                  LogOut
+                </button>
+                <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
+                  Delete Account
+                </button>
+              </div>
             </div>
 
-            <div className="text-center mt-4">
-              <h3 className="text-[18px] leading-[30px] text-headingColor font-bold ">
-                Dhruvil Patel
-              </h3>
-              <p className="text-textColor text-[15px] leading-6 font-medium">
-                Example@gmail.com
-              </p>
+            <div className="md:col-span-2 md:px-[30px]">
+              <div>
+                <button
+                  onClick={() => setTab("bookings")}
+                  className={` ${
+                    tab === "bookings" &&
+                    "bg-primaryColor text-white font-normal"
+                  }p-2 py-1 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
+                >
+                  My Bookings
+                </button>
+                <button
+                  onClick={() => setTab("settings")}
+                  className={` ${
+                    tab === "settings" &&
+                    "bg-primaryColor text-white font-normal"
+                  }p-2 py-1 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
+                >
+                  Profile Settings
+                </button>
+              </div>
 
-              <p className="text-textColor text-[15px] leading-6 font-medium">
-                Blood Type:
-                <span className="ml-2 text-headingColor text-[22px] leading-8">
-                  O+
-                </span>
-              </p>
-            </div>
-
-            <div className="mt-[50px] md:mt-[100px]">
-              <button
-                onClick={handlelogout}
-                className="w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white"
-              >
-                LogOut
-              </button>
-              <button className="w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white">
-                Delete Account
-              </button>
+              {tab === "bookings" && <MyBookings />}
+              {tab === "settings" && <Profile />}
             </div>
           </div>
-
-          <div className="md:col-span-2 md:px-[30px]">
-            <div>
-              <button
-                onClick={() => setTab("bookings")}
-                className={` ${
-                  tab === "bookings" && "bg-primaryColor text-white font-normal"
-                }p-2 py-1 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
-              >
-                My Bookings
-              </button>
-              <button
-                onClick={() => setTab("settings")}
-                className={` ${
-                  tab === "settings" && "bg-primaryColor text-white font-normal"
-                }p-2 py-1 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor`}
-              >
-                Profile Settings
-              </button>
-            </div>
-
-            {tab === "bookings" && <MyBookings />}
-            {tab === "settings" && <Profile />}
-          </div>
-        </div>
+        )}
       </div>
     </section>
   );
--- frontend/src/Dashboard/user-account/MyBookings.jsx
+++ frontend/src/Dashboard/user-account/MyBookings.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import userFetchData from "../../hooks/userFetchData";
+import { BASE_URL } from "../../config";
+import DoctorCard from "../../components/Doctors/DoctorCard";
+import Loading from "../../components/Loader/Loading";
+import Error from "../../components/Error/Error";
 
-function MyBookings() {
-  return <div>MyBookings</div>;
-}
+const MyBookings = () => {
+  const {
+    data: appointments,
+    loading,
+    error,
+  } = userFetchData(`${BASE_URL}/users/appointments/my-appointments`);
+  return (
+    <div>
+      {loading && !error && <Loading />}
+
+      {error && !loading && <Error errMessage={error} />}
+
+      {!loading && !error && (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+          {appointments.map((doctor) => (
+            <DoctorCard doctor={doctor} key={doctor._id} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default MyBookings;
