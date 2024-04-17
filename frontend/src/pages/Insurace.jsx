31e03346042f8364d6d36aa2077d33e89322fe60
Dhruvil patel Ordex <144197809+Dhruvil-patel09@users.noreply.github.com>
2024-04-17 19:28:19 +0530

    insurance card add

--- frontend/src/pages/Insurace.jsx
+++ frontend/src/pages/Insurace.jsx
@@ -1,46 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Import Link from React Router
-import { insurance } from "../assets/data/insurace"; // corrected import path
-
+import { Link } from "react-router-dom";
+import { insurance } from "../assets/data/insurace"; // Corrected import path
+import "./css/insurance.css";
 function InsurancePage() {
   return (
-    <div
-      style={{
-        margin: `50px`,
-        display: `flex`,
-        flexDirection: `row`,
-        justifyContent: `center`,
-      }}
-    >
+    <div className="articles">
       {insurance.map((company) => (
-        <Link
-          key={company.id}
-          to={company.link}
-          style={{ textDecoration: "none" }}
-          target="_blank"
-        >
-          <div
-            className="e-card e-card-horizontal"
-            style={{ width: `400px`, margin: `10px` }}
-          >
-            <img
-              src={company.photo}
-              alt={company.name}
-              style={{ width: `100%`, height: "auto" }}
-            />
-            <div className="e-card-stacked">
-              <div className="e-card-header">
-                <div className="e-card-header-caption">
-                  <div className="e-card-header-title">{company.name}</div>
-                </div>
-              </div>
-              <div className="e-card-content">
-                {/* You can add more properties of the insurance company here if needed */}
-                {/* For example: {company.description} */}
-              </div>
+        <article key={company.id}>
+          <div className="article-wrapper">
+            <figure>
+              <img src={company.photo} alt={company.name} />
+            </figure>
+            <div className="article-body">
+              <h2>{company.name}</h2>
+              <p>
+                Curabitur convallis ac quam vitae laoreet. Nulla mauris ante,
+                euismod sed lacus sit amet, congue bibendum eros. Etiam mattis
+                lobortis porta. Vestibulum ultrices iaculis enim imperdiet
+                egestas.
+              </p>
+              <Link to={company.link} className="read-more">
+                Read more <span className="sr-only">about {company.name}</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="icon"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </Link>
             </div>
           </div>
-        </Link>
+        </article>
       ))}
     </div>
   );
